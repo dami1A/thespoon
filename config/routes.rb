67e11ab009f03d6f3361b625384f7506c95d3776
damien arnaud <darnaud@mail.com>
2019-02-12 10:43:00 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'home', to: 'pages#home'
+  root to: 'pages#home'
+  # #READ ALL
+  # get 'restaurants', to: 'restaurants#index'
+  # #CREATE
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # # READ ONE
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # # UPDATE
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # # DELETE
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+  resources :restaurants
 end
